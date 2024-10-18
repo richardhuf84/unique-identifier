52684951e02f2e003f14daa0d2ef86eea74e6506
Richard Huf <richardhuf84@gmail.com>
2024-10-18 18:21:47 +1100

    chore: implement generateUniqueIdentifier with simple node crypto.randomUUID()

--- src/lib/generateUniqueIdentifier.spec.ts
+++ src/lib/generateUniqueIdentifier.spec.ts
@@ -1,83 +1,11 @@
 import {
-  INDIVIDUAL_CHARACTER_REGEXP,
-  CHARACTER_RANGE_REGEXP,
   generateUniqueIdentifier
 } from './generateUniqueIdentifier';
+import { validate as uuidValidate } from 'uuid';
 
-describe('uniqueIdentifier', () => {
-  it('should be comprised of 5 random strings, separated by a hyphen', () => {
-    console.log('*******/n', generateUniqueIdentifier())
-    const parts = generateUniqueIdentifier().split('-');
-    expect(parts).toHaveLength(5);
-    expect(parts[0]).toHaveLength(8)
-    expect(parts[1]).toHaveLength(4)
-    expect(parts[2]).toHaveLength(4)
-    expect(parts[3]).toHaveLength(4)
-    expect(parts[4]).toHaveLength(12)
+
+describe('generateUniqueIdentifier', () => {
+  it('should return a valid UUID v4', () => {
+    expect(uuidValidate(generateUniqueIdentifier())).toBeTruthy();
   });
-
-  test.each`
-  index | expected | extra
-  ${0} | ${8}  | ${'first'}
-  ${1} | ${4}  | ${'second'}
-  ${2} | ${4}  | ${'third'}
-  ${3} | ${4}  | ${'fourth'}
-  ${4} | ${12} | ${'fifth'}
-  `
-    (`the $extra segment should be $expected characters in length`, ({ index, expected, extra }) => {
-      const parts = generateUniqueIdentifier().split('-');
-      console.log(parts, index)
-      expect(parts[index]).toHaveLength(expected)
-    });
-
-  it('each segment should be comprised of lowercase letters, uppercase letters or numbers 0-9', () => {
-    const parts = generateUniqueIdentifier().split('-');
-    parts.forEach(part => {
-      expect(CHARACTER_RANGE_REGEXP.test(part)).toBeTruthy();
-    })
-  })
-});
-
-
-describe('INDIVIDUAL_CHARACTER_REGEXP', () => {
-  test.each`
-  a | expected
-  ${'a'} | ${true}
-  ${'A'} | ${true}
-  ${'0'} | ${true}
-  ${'9'} | ${true}
-  `
-    (`$a is an accepted character`, ({ a, expected }) => {
-      expect(INDIVIDUAL_CHARACTER_REGEXP.test(a)).toBe(expected);
-    });
-
-  test.each`
-  a | expected
-  ${'*'} | ${false}
-  ${'?'} | ${false}
-  ${'~'} | ${false}
-  ${'😀'} | ${false}
-  `
-    (`$a is not an accepted character`, ({ a, expected }) => {
-      expect(INDIVIDUAL_CHARACTER_REGEXP.test(a)).toBe(expected);
-    });
-});
-describe('CHARACTER_RANGE_REGEXP', () => {
-  test.each`
-  a | expected
-  ${'aA0'} | ${true}
-  ${'d93kg0s'} | ${true}
-  `
-    (`$a is an accepted character`, ({ a, expected }) => {
-      expect(CHARACTER_RANGE_REGEXP.test(a)).toBe(expected);
-    });
-
-  test.each`
-  a | expected
-  ${'ag*'} | ${false}
-  ${'9876@ffg'} | ${false}
-  `
-    (`$a is not an accepted character`, ({ a, expected }) => {
-      expect(INDIVIDUAL_CHARACTER_REGEXP.test(a)).toBe(expected);
-    });
 });
--- src/lib/generateUniqueIdentifier.ts
+++ src/lib/generateUniqueIdentifier.ts
@@ -5,11 +5,6 @@ try {
   console.error(err);
 }
 
-const DEFAULT_UINT8_ARRAY_LENGTH = 256;
-
-export const INDIVIDUAL_CHARACTER_REGEXP = /^[a-zA-Z0-9]{1}$/;
-export const CHARACTER_RANGE_REGEXP = /[a-zA-Z0-9]/
-
-export const generateUniqueIdentifier = (uInt8ArrayLength: number = DEFAULT_UINT8_ARRAY_LENGTH) =>
-  Buffer.from(crypto.randomFillSync(new Uint8Array(uInt8ArrayLength)).buffer).toString('base64');
+export const generateUniqueIdentifier = () =>
+  crypto.randomUUID();
 
