b3066815e6a6252bb5a98a0a74f4ddaad0803e0a
Richard Huf <richardhuf84@gmail.com>
2024-10-18 18:29:31 +1100

    chore: add jsdoc and delete package-lock

--- src/lib/generateUniqueIdentifier.spec.ts
+++ src/lib/generateUniqueIdentifier.spec.ts
@@ -3,7 +3,10 @@ import {
 } from './generateUniqueIdentifier';
 import { validate as uuidValidate } from 'uuid';
 
-
+/** 
+ * Generate a RFC4122 compliant UUID v4
+ * returns @string unique identifier
+ */
 describe('generateUniqueIdentifier', () => {
   it('should return a valid UUID v4', () => {
     expect(uuidValidate(generateUniqueIdentifier())).toBeTruthy();
